3bd2c815bedbcd13cb4694edac70c36f313d8da4
Ultraman-Mark <2278625687@qq.com>
2021-07-23 20:36:19 +0800

    spring boot run

--- src/main/java/com/pyl/Springboot/Coupons/SpringBootApplication.java
+++ src/main/java/com/pyl/Springboot/Coupons/SpringBootApplication.java
@@ -1,17 +1,28 @@
 package com.pyl.Springboot.Coupons;
 
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 //应用启动类
+//@org.springframework.boot.autoconfigure.SpringBootApplication
 public class SpringBootApplication {
     public static void main(String[] args) {
-        //1.通过静态run方法
-        SpringApplication.run(SpringBootApplication.class,args);
+//        1.通过静态run方法
+//        SpringApplication.run(SpringBootApplication.class,args);
 
-        //2.通过api调整应用行为
-        //SpringApplication application = new SpringApplication(SpringBootApplication.class);
-        //application.run(args);
-
+//        2.通过api调整应用行为
+//        SpringApplication application = new SpringApplication(SpringBootApplication.class);
+//        application.setBannerMode(Banner.Mode.OFF);
+//        application.setWebApplicationType(WebApplicationType.NONE);
+//        application.run(args);
+//
+//        3.SpringApplicationBuilder Fluent Api,链式调用
+//        new SpringApplicationBuilder(SpringBootApplication.class)
+//                .bannerMode(Banner.Mode.OFF)
+//                .web(WebApplicationType.NONE)
+//                .run(args);
     }
 }
