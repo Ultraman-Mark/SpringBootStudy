5ca1822b0845a3179536ad246beb20a0dc797783
Ultraman-Mark <2278625687@qq.com>
2021-07-23 22:57:03 +0800

    spring boot run

--- src/main/java/com/pyl/Springboot/Coupons/SpringBootStudyApplication.java
+++ src/main/java/com/pyl/Springboot/Coupons/SpringBootStudyApplication.java
@@ -1,26 +1,26 @@
 package com.pyl.Springboot.Coupons;
 
-
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 //应用启动类
-//@org.springframework.boot.autoconfigure.SpringBootApplication
-public class SpringBootApplication {
+@SpringBootApplication
+public class SpringBootStudyApplication {
     public static void main(String[] args) {
 //        1.通过静态run方法
-//        SpringApplication.run(SpringBootApplication.class,args);
+//        SpringApplication.run(SpringBootStudyApplication.class,args);
 
 //        2.通过api调整应用行为
-//        SpringApplication application = new SpringApplication(SpringBootApplication.class);
+//        SpringApplication application = new SpringApplication(SpringBootStudyApplication.class);
 //        application.setBannerMode(Banner.Mode.OFF);
 //        application.setWebApplicationType(WebApplicationType.NONE);
 //        application.run(args);
 //
 //        3.SpringApplicationBuilder Fluent Api,链式调用
-//        new SpringApplicationBuilder(SpringBootApplication.class)
+//        new SpringApplicationBuilder(SpringBootStudyApplication.class)
 //                .bannerMode(Banner.Mode.OFF)
 //                .web(WebApplicationType.NONE)
 //                .run(args);
