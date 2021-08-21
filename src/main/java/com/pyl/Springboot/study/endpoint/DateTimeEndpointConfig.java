3c7ebd957d4377072fec3076f9561bc3e089c6ed
Ultraman-Mark <2278625687@qq.com>
2021-08-21 21:05:22 +0800

    3.10-Actuator监控

--- src/main/java/com/pyl/Springboot/study/endpoint/DateTimeEndpointConfig.java
+++ src/main/java/com/pyl/Springboot/study/endpoint/DateTimeEndpointConfig.java
@@ -1,7 +1,6 @@
 package com.pyl.Springboot.study.endpoint;
 
 import org.springframework.boot.actuate.autoconfigure.endpoint.condition.ConditionalOnAvailableEndpoint;
-import org.springframework.boot.actuate.autoconfigure.endpoint.condition.ConditionalOnEnabledEndpoint;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +11,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DateTimeEndpointConfig {
 
+    /**
+     * 视频中的@ConditionalOnEnabledEndpoint注解在2.1.4版本中存在，
+     * 但2.3.11版本中只找到@ConditionalOnAvailableEndpoint注解
+     * 还不太清楚二者的不同，但测试数据的使用还是正常的，推测可能在2.3版本对注解进行了更换
+     */
     @Bean
     @ConditionalOnMissingBean
     @ConditionalOnAvailableEndpoint
-    @ConditionalOnEnabledEndpoint
+//    @ConditionalOnEnabledEndpoint
     public DateTimeEndPoint dateTimeEndPoint(){
         return new DateTimeEndPoint();
     }
